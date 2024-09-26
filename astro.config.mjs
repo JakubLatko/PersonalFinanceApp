f09efe5540238fe85f3571c6b415cea54e7e7f11
Jakub <jakub.latko@vp.pl>
2024-09-26 11:57:58 +0200

    mobile side bar started

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
+  devToolbar:{
+    enabled:false,
+  },
   output: 'server',
   adapter: netlify()
 });
